a39fc76fb77c7dc14a44a8fbe524d27ad0b1c5f9
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-05-16 16:34:12 +0900

    sorceryでユーザー機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
+  resources :users
+  get '/login', to: 'user_sessions#new'
+  post '/login', to: 'user_sessions#create'
+  post '/logout', to: 'user_sessions#destroy'
 end
