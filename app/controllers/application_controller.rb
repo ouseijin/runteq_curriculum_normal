3743d83cf44564312cfa4f4f67c6528ab124ef61
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-05-25 15:09:52 +0900

    掲示板作成機能2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-    add_flash_types :success, :info, :warning, :danger
-    before_action :require_login
+  add_flash_types :success, :info, :warning, :danger
+  before_action :require_login
 
-    private
+  private
 
-    def not_authenticated
-      flash[:warning] = 'ログインしてください'
-      redirect_to login_url
-    end
+  def not_authenticated
+    flash[:warning] = 'ログインしてください'
+    redirect_to login_url
+  end
 end
