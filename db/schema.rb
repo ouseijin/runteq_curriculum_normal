00d255789eb24ef9481bb8234862a83f4c3bf381
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-05-28 16:51:58 +0900

    掲示板の画像アップロード機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_124004) do
+ActiveRecord::Schema.define(version: 2021_05_28_063826) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_05_20_124004) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "board_image"
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
