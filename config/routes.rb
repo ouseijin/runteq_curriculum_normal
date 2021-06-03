993b5a0a7be5173d2a6151c85cf5772fe1b8ecbb
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-06-03 21:05:35 +0900

    掲示板の編集、削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
-  resources :boards, only: %i[index new create show] do
+  resources :boards do
     resources :comments, only: %i[create], shallow: true
   end
 end
