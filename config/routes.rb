83d3f1e86706fb1a2cf5cc9301740fec28682995
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-06-08 15:33:51 +0900

    ブックマーク機能の追加3

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'user_sessions#destroy'
   resources :boards do
     resources :comments, only: %i[create], shallow: true
-    resource :bookmarks, only: %i[create, destroy]
+    resource :bookmarks, only: %i[create destroy]
     collection do
       get :bookmarks
     end
